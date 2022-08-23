88e949ae8cfb9055465b7274d20de874a42f4684
gin-guo <ginnyguoshi@gmail.com>
2022-08-23 17:28:27 -0400

    added youtube link lol

--- main/main/Test/Test.ino
+++ main/main/Test/Test.ino
@@ -1,6 +1,8 @@
 int IRSensor = A5; // connect ir sensor to arduino pin 2
 int LED = 13; // conect Led to arduino pin 13
 
+// youtube: https://www.youtube.com/watch?v=FgiSbwKplmU&ab_channel=PraveenDehari
+
 int sensorValue = 0;
 
 void setup() 
