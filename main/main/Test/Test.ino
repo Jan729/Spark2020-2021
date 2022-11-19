929a350be9e147126ce3e5c14c6a9a3e69ce9a59
gin-guo <ginnyguoshi@gmail.com>
2022-11-19 13:33:26 -0500

    updated ir sensor code with shift register declarations

--- main/main/Test/Test.ino
+++ main/main/Test/Test.ino
@@ -1,13 +1,18 @@
-int IRSensor = A5; // connect ir sensor to arduino pin A5
-int LED = 12; // conect Led to arduino pin 12
-
+// REFERENCES:
 // circuit: https://www.youtube.com/watch?v=FgiSbwKplmU&ab_channel=PraveenDehari
 // code: https://learn.adafruit.com/ir-breakbeam-sensors/arduino
 
+// PIN DECLARATIONS
+int IRSensor = A5; // connect ir sensor to arduino pin A5
+int LED = 12; // connect Led to arduino pin 12
+int latchPin = 5;  // latch pin of shift register
+int clockPin = 6; // clock pin of shift register
+int dataPin = 4;  // data pin of shift register
 
-
-int sensorValue = 0, lastState = 0;
+// GLOBAL VARIABLES 
+int sensorValue = 0;
 int threshold = 200;
+
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -16,6 +21,10 @@ void setup() {
   pinMode(LED, OUTPUT); // led pin as OUTPUT
   pinMode(IRSensor, INPUT); // ir sensor pin as INPUT
   digitalWrite(IRSensor, HIGH); // turn on the pullup
+  // shift registers
+  pinMode(latchPin, INPUT); // set all the pins of 74HC595 as INPUT
+  pinMode(dataPin, INPUT);  
+  pinMode(clockPin, INPUT);
 }
 
 
@@ -33,13 +42,4 @@ void loop() {
     digitalWrite(LED, LOW);
   }
 
-
-//  if (IRSensor && !lastState) {
-//    Serial.println("Unbroken");
-//  } 
-//  if (!IRSensor && lastState) {
-//    Serial.println("Broken");
-//  }
-//  
-//  lastState = IRSensor;
 }
