57d47bef1a53586d123c83ae9e99fdb9131d9631
gin-guo <72530527+gin-guo@users.noreply.github.com>
2023-10-02 20:44:22 -0400

    mux test code

---
+++ main/main/ir_sensors_with_muxes/ir_sensors_with_muxes.ino
@@ -0,0 +1,37 @@
+#define A1PIN 2 // select pin A
+#define B1PIN 3 // select pin B
+#define C1PIN 4 // select pin C
+#define A2PIN 5 // select pin A
+#define B2PIN 6 // select pin B
+#define C2PIN 7 // select pin C
+#define IOPIN A0
+
+int ir_reading = 0;
+
+void setup() {
+  pinMode(A1PIN, OUTPUT);
+  pinMode(B1PIN, OUTPUT);
+  pinMode(C1PIN, OUTPUT);
+
+  pinMode(A2PIN, OUTPUT);
+  pinMode(B2PIN, OUTPUT);
+  pinMode(C2PIN, OUTPUT);
+  
+  pinMode(IOPIN, INPUT);
+  
+  // signal pins: little endian, A is least significant
+  digitalWrite(A1PIN, LOW);
+  digitalWrite(B2PIN, LOW);
+  digitalWrite(C1PIN, LOW);
+
+  digitalWrite(A2PIN, LOW);
+  digitalWrite(B2PIN, LOW);
+  digitalWrite(C2PIN, LOW);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  ir_reading = analogRead(IOPIN);
+  Serial.println(ir_reading);
+}
