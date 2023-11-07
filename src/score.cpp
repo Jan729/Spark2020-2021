f0b6dcc27e509c8e77809dab58baa4e9b00fc499
gin-guo <ginnysh.guo@mail.utoronto.ca>
2023-11-06 21:32:19 -0500

    update score function and hex display code

--- src/score.cpp
+++ src/score.cpp
@@ -1,58 +1,54 @@
 #include <Arduino.h>
 #include "functions.h"
 #include "global-variables.h"
+#include "TM1637Display.h"
 
-void updateScore() { 
-  if (!loseGame) {
-    targetDifficulty += 1;
+// using 2 four digit 7-segment displays –> one for current score, one for high score
+// use TM1637Display library (installed as a .cpp and .h file in the src directory)
+// https://github.com/avishorp/TM1637
+// CLK and DIO pins need to be connected to Arduino
+
+// pin declarations just for reference – to be removed?
+#define curScoreCLK 2
+#define curScoreDIO 3
+
+#define highScoreCLK 4
+#define highScoreDIO 5
+
+// create new instance of the TM1637Display class by passing clk and dio pin values to it
+TM1637Display curScoreDisplay(curScoreCLK, curScoreDIO);
+TM1637Display highScoreDisplay(highScoreCLK, highScoreDIO)
+
+display.setBrightness(7); // brightness level from 0 (lowest) to 7 (highest)
+
+void updateScore(score, targetDifficulty, finishTime) {
+  // calculate score given based on level difficulty and time taken to complete level
+  // level difficulty increases incrementally by 1 after each level
+  targetDifficulty += 1;
+  score += 5 * targetDifficulty * (2 - (finishTime/200000)) // score is constant if user completes level after 3 minutes
+                                                            // https://www.desmos.com/calculator/xxpl1mwy9a
+  displayScore(score);
+  sethighScore(score);
+>>>>>>> Stashed changes
+}
+
+void displayScore(score) {
+  curScoreDisplay.clear(); // clear previous value
+  delay(500); // delay to make score update more obvious to player
+  curScoreDisplay.showNumberDec(score, false); // display new score <- need to test this
+}
+
+void sethighScore(score, highscore) {
+  if (score > highscore) {
+    highscore = score;
+    highScoreDisplay.clear();
+    delay(500);
+    highScoreDisplay.showNumberDec(highscore, false);
+    highScoreDisplay.clear();
+    delay(500); // flash new high score
+    highScoreDisplay.showNumberDec(highscore, false);
+    highScoreDisplay.clear();
+    delay(500);
+    highScoreDisplay.showNumberDec(highscore, false);
   }
-  score += (30000 - finishTime)/1500 * targetDifficulty/5 ; //TODO: finish this code
-  
-  displayScore();
-  sethighScore();
-}
-
-void displayScore() {
-  int firstDigit = score/100;
-  int secondDigit = (score-firstDigit)/10;
-  int thirdDigit = (score-firstDigit-secondDigit);
-
-  sevseg1.setNumber(firstDigit);
-  sevseg1.refreshDisplay(); 
-  sevseg2.setNumber(secondDigit);
-  sevseg2.refreshDisplay(); 
-  sevseg3.setNumber(thirdDigit);
-  sevseg3.refreshDisplay(); 
-  
-  
-  //update digits on the seven seg display 
-
-// TODO: update this code to work with our current parts
-
-  // Wire.beginTransmission(0x20); //select chip 0x20
-  // Wire.write(0x12); // Select Port A
-  // Wire.write(gpio_num_array[thirdDigit]); // Send data
-  // Wire.endTransmission();
-  // Wire.beginTransmission(0x20); //select chip 0x20
-  // Wire.write(0x13); // Select Port B
-  // Wire.write(gpio_num_array[highFirst]); // Send data
-  // Wire.endTransmission();
-  // Wire.beginTransmission(0x27); //select chip 0x27
-  // Wire.write(0x12); // Select Port A
-  // Wire.write(gpio_num_array[highSecond]); // Send data
-  // Wire.endTransmission();
-  // Wire.beginTransmission(0x27); //select chip 0x27
-  // Wire.write(0x13); // Select Port B
-  // Wire.write(gpio_num_array[highThird]); // Send data
-  // Wire.endTransmission();
-
-}
-
-void sethighScore() {
-  // if (score > highscore) {
-  //   highscore = score;
-  //   int highFirst = high/100;
-  //   int highSecond = (highscore-highFirst)/10;
-  //   int highThird = (highscore-highFirst-highSecond);
-  // }
 }
